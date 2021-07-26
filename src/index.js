59ff93ace073822748c31fdf3ba74121b743e50e
Artur Kutusenko <kutusenko@gmail.com>
2021-07-26 20:24:40 +0300

    add match function

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import genreMarkup from './templates/movie-genre.hbs';
 import ApiServise from './js/api-service';
 
 const apiServise = new ApiServise();
-let GENRES = apiServise.fetchGenres();
-console.log(GENRES);
+
+// let GENRES = [];
+// apiServise.fetchGenres().then(arr => {
+//   console.log(arr);
+// });
+// console.log(GENRES);
 
 apiServise.fetchPopularMovies().then(results => {
   renderMarkup(results);
@@ -84,4 +88,35 @@ function findGenreById(arr) {
   }
 
   return genres;
+}
+
+function match(arr) {
+  const genresBD = [
+    { id: 28, name: 'Action' },
+    { id: 12, name: 'Adventure' },
+    { id: 16, name: 'Animation' },
+    { id: 35, name: 'Comedy' },
+    { id: 80, name: 'Crime' },
+    { id: 99, name: 'Documentary' },
+    { id: 18, name: 'Drama' },
+    { id: 10751, name: 'Family' },
+    { id: 14, name: 'Fantasy' },
+    { id: 36, name: 'History' },
+    { id: 27, name: 'Horror' },
+    { id: 10402, name: 'Music' },
+    { id: 9648, name: 'Mystery' },
+    { id: 10749, name: 'Romance' },
+    { id: 878, name: 'Science Fiction' },
+    { id: 10770, name: 'TV Movie' },
+    { id: 53, name: 'Thriller' },
+    { id: 10752, name: 'War' },
+    { id: 37, name: 'Western' },
+  ];
+  for (let i = 0; i < arr.length; i += 1) {
+    genresBD.forEach(item => {
+      if (arr[i].textContent == item.id) {
+        arr[i].textContent = item.name;
+      }
+    });
+  }
 }
