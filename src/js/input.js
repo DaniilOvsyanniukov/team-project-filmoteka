dd148c5952ff923830a57168c5818732ee19128a
Artur Kutusenko <kutusenko@gmail.com>
2021-07-27 22:49:41 +0300

    add some changes)

---
+++ src/js/input.js
@@ -0,0 +1,101 @@
+import refs from './refs';
+import movieMarkup from '../templates/movie-card';
+import genreMarkup from '../templates/movie-genre.hbs';
+import ApiServise from './api-service';
+
+const apiServise = new ApiServise();
+
+// let GENRES = [];
+// apiServise.fetchGenres().then(arr => {
+//   console.log(arr);
+// });
+// console.log(GENRES);
+
+apiServise
+  .fetchPopularMovies()
+  .then(results => {
+    renderMarkup(results);
+    // const movieId = document.querySelector('.movie-id');
+    // movieId.insertAdjacentHTML(
+    //   'beforeend',
+    //   genreMarkup(results.map(result => findGenreById(result.genre_ids))),
+    // );
+    console.log(
+      results.map(r => {
+        return (r.genre_ids = findGenreById(r.genre_ids));
+      }),
+    );
+  })
+  .catch(error => console.log(error));
+
+refs.searchForm.addEventListener('submit', onSearchForm);
+
+function onSearchForm(e) {
+  e.preventDefault();
+  apiServise.query = e.currentTarget.elements.query.value;
+
+  if (apiServise.query.trim() === '') {
+    return alert('Пожалуйста, введите ваш запрос');
+  }
+
+  clearGallery();
+  clearInput(e);
+  apiServise.resetPage();
+  apiServise
+    .fetchMoviesByRequest()
+    .then(results => {
+      renderMarkup(results);
+    })
+    .catch(error => console.log(error));
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+function clearInput(e) {
+  e.currentTarget.elements.query.value = '';
+}
+
+function renderMarkup(results) {
+  refs.gallery.insertAdjacentHTML('beforeend', movieMarkup(results));
+}
+
+function findGenreById(arr) {
+  const genresBD = [
+    { id: 28, name: 'Action' },
+    { id: 12, name: 'Adventure' },
+    { id: 16, name: 'Animation' },
+    { id: 35, name: 'Comedy' },
+    { id: 80, name: 'Crime' },
+    { id: 99, name: 'Documentary' },
+    { id: 18, name: 'Drama' },
+    { id: 10751, name: 'Family' },
+    { id: 14, name: 'Fantasy' },
+    { id: 36, name: 'History' },
+    { id: 27, name: 'Horror' },
+    { id: 10402, name: 'Music' },
+    { id: 9648, name: 'Mystery' },
+    { id: 10749, name: 'Romance' },
+    { id: 878, name: 'Science Fiction' },
+    { id: 10770, name: 'TV Movie' },
+    { id: 53, name: 'Thriller' },
+    { id: 10752, name: 'War' },
+    { id: 37, name: 'Western' },
+  ];
+  const genres = [];
+
+  arr.forEach(el => {
+    genresBD.forEach(genre => {
+      if (el === genre.id) {
+        genres.push(genre.name);
+      }
+    });
+  });
+
+  if (genres.length > 2) {
+    genres.splice(2, 5, 'other');
+  }
+
+  return genres;
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,9 +1,7 @@
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-const movieAbout = document.querySelector('.movie-id');
 
 export default {
   searchForm,
   gallery,
-  movieAbout,
 };
