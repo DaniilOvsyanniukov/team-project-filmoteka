f712660403e5dea7d48858a4a788a62455695f87
Anastasiia Sanatar <sanatar.nastya@gmail.com>
2021-07-28 13:49:11 +0300

    разметка и стили модалки для десктопа + немного js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
 import './sass/main.scss';
+//import './js/components/footer-modal';
 
 
-const refs = {
- // openFooterModal: document.querySelector('.footer-open-modal'),
-    openFooterModal: document.querySelector('[data-action="open-lightbox"]'),
-  closeFooterModal: document.querySelector('[data-action="close-lightbox"]'),
-  lightboxFooterModal: document.querySelector('.js-lightbox'),
-};
-
-//================================открыть модалку ========================================
-console.log(refs.openFooterModal);
-refs.openFooterModal.addEventListener('click', onOpenModal);
-
-function onOpenModal() {
-   
-    refs.lightboxFooterModal.classList.remove('visually-hidden');
-}
-//================================закрыть модалку ========================================
-
-// refs.closeFooterModal.addEventListener('click', closeModal);
-
-// function closeModal() {
-   
-//     refs.lightboxFooterModal.classList.add('visually-hidden');
-// }
--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,8 +1,10 @@
+
 const refs = {
-  //openFooterModal: document.querySelector('.footer-open-modal'),
+ 
     openFooterModal: document.querySelector('[data-action="open-lightbox"]'),
-  closeFooterModal: document.querySelector('[data-action="close-lightbox"]'),
-  lightboxFooterModal: document.querySelector('.js-lightbox'),
+    closeFooterModal: document.querySelector('[data-action="close-lightbox"]'),
+    lightboxFooterModal: document.querySelector('.js-lightbox'),
+    backdropClick: document.querySelector('.modal-background'),
 };
 
 //================================открыть модалку ========================================
@@ -10,8 +12,37 @@ const refs = {
 refs.openFooterModal.addEventListener('click', onOpenModal);
 
 function onOpenModal() {
-    
-       refs.lightboxFooterModal.classList.remove('.visually-hidden');
-    
-    
+   
+    refs.lightboxFooterModal.classList.remove('visually-hidden');
+}
+
+//========================закрыть модалку через иконку ========================================
+
+refs.closeFooterModal.addEventListener('click', onCloseModal);
+
+function onCloseModal() {
+   window.removeEventListener('keydown', onEscClick); //для закрытия по ESС
+    refs.lightboxFooterModal.classList.add('visually-hidden');
+     
+}
+
+// ==============================закрыть по backdrop====================
+
+refs.backdropClick.addEventListener('click', onBackdropClick);
+
+function onBackdropClick() {
+    onCloseModal();
+
+    console.log('кликнули по backdrop');
+}
+
+// ===========================закрыть по ESС==========================
+
+function onEscClick(event) {
+    const ESC_KEY_CODE = 'Escape';
+    console.log(event.code);
+
+    if (event.code === ESC_KEY_CODE) {
+        onCloseModal();
+    }
 }
