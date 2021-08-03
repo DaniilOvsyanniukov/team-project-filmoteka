fde3fa3ef55d108aa30fbb94f1554b07ed0a66fb
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-08-03 18:05:43 +0300

    library-10
    добавил бесконечный скролл.

--- src/js/library.js
+++ src/js/library.js
@@ -11,6 +11,11 @@ const section = document.querySelector('.gall-js');
 const searchForm = document.querySelector('.search-form-js');
 const homePageLink = document.querySelector('.home-js');
 const header = document.querySelector('.header-js');
+const sentinel = document.getElementById('sentinel');
+
+let watchedQueueFlag = true;
+let visualNumberOfItems = 6;
+let startIndex = 6;
 
 //Вешаю слушателей на кнопки и мою библиотеку
 libraryLink.addEventListener('click', onLibraryLinkCLick);
@@ -24,6 +29,8 @@ function onLibraryLinkCLick(event) {
   plaginationEl.style.display = 'none';
   listOfHeaderBtns.classList.remove('visually-hidden');
   section.classList.add('section-library-height');
+  sentinel.classList.remove('display-none');
+  watchedQueueFlag = true;
   // изменяет хедер визуально
   //yarik
   searchForm.classList.add('visually-hidden');
@@ -34,8 +41,8 @@ function onLibraryLinkCLick(event) {
   //
   // вызывает функцию которая рендерит карточки из локалсторедж (renderMarkupFromLocalStorage)
   // вызывает функцию проверку, если локалсторедж пуст, то на галерею вешается фоновый рисунок и надпись No movies (heightForGalleryBackgroundImg)
-  renderMarkupFromLocalStorage('watched movies');
-  heightForGalleryBackgroundImg();
+  renewParam(6);
+  firstSixMovies('watched movies');
 }
 
 //функция обрабатвает события на кнопках
@@ -44,41 +51,18 @@ function onListOfHeadersBtns(event) {
   //если кнопка вотчед, то тогда рендерятся карточки из локалсторедж под ключом вотчед
   // если нет информации в соответствующем ключе, то вещается фоновый рисунок
   if (event.target.classList.contains('watched-js')) {
-    renderMarkupFromLocalStorage('watched movies');
-    heightForGalleryBackgroundImg();
+    watchedQueueFlag = true;
+    renewParam(6);
+
+    firstSixMovies('watched movies');
   }
   //если кнопка кьюю то тогда рендерятся карточки из ключа кьюю
   // если нет информации в соответствующем ключе, то вещается фоновый рисунок
   if (event.target.classList.contains('queue-js')) {
-    renderMarkupFromLocalStorage('In queue');
-    heightForGalleryBackgroundImg();
-  }
-}
+    watchedQueueFlag = false;
 
-// функция рендеринга карточек,
-function renderMarkupFromLocalStorage(key) {
-  // очищаем галерею
-  gallery.innerHTML = '';
-  // обращаемся в локал сторедж с нужнм для нас ключем
-  const moviesFromLocalStorage = JSON.parse(localStorage.getItem(key));
-  // обрезаем жанры до двух, согласно макету
-  const updateInfoFroLocalStarage = sliceGanresDate(moviesFromLocalStorage);
-  // добавляем информацию в шаблон и закидываем все в галерею
-  gallery.insertAdjacentHTML('beforeend', movieMarkup(updateInfoFroLocalStarage));
-}
-
-// функция для проверки есть ли под заданнм ключем информация в локал сторедж
-function heightForGalleryBackgroundImg() {
-  // создаем коллекцию карточек после рендеринга
-  const arrOfCards = gallery.querySelectorAll('.movie-card-js');
-  // если такой коллекции нет, то есть ее длина равна 0, то добавляем класс galleryEmptySpace и показываем заголовок
-  // в любом другом случае убираем класс и прячем заголовок
-  if (arrOfCards.length === 0) {
-    gallery.classList.add('galleryEmptySpace');
-    titleNoMovie.classList.remove('display-none');
-  } else {
-    gallery.classList.remove('galleryEmptySpace');
-    titleNoMovie.classList.add('display-none');
+    renewParam(6);
+    firstSixMovies('In queue');
   }
 }
 
@@ -91,10 +75,71 @@ function sliceGanresDate(arr) {
     if (item.genres.length > 2) {
       item.genres.splice(2, 5);
       item.genres.splice(1, 0, { name: `, ` });
+    } else if (item.genres.length === 2) {
+      item.genres.splice(1, 0, { name: `, ` });
     }
     if (item.release_date) {
       item.release_date = item.release_date.slice(0, 4);
     }
     return item;
   });
+}
+//============================================= infinity scroll==================
+
+const loadMore = function (key) {
+  const moviesFromLocalStorage = JSON.parse(localStorage.getItem(key));
+  let numberOfItems = 6;
+  visualNumberOfItems += numberOfItems;
+  if (moviesFromLocalStorage === null) {
+    return;
+  }
+  const visualItems = moviesFromLocalStorage.slice(startIndex, visualNumberOfItems);
+
+  console.log('visualItems', visualItems);
+
+  gallery.insertAdjacentHTML('beforeend', movieMarkup(sliceGanresDate(visualItems)));
+  startIndex += numberOfItems;
+};
+
+function onEntry(entries) {
+  console.log(entries);
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log(entry);
+      if (watchedQueueFlag) {
+        loadMore('watched movies');
+      } else {
+        loadMore('In queue');
+      }
+    }
+  });
+}
+const options = {
+  threshold: 0.8,
+  rootMargin: '0px 0px 50px 0px',
+};
+
+const interObserv = new IntersectionObserver(onEntry, options);
+
+interObserv.observe(sentinel);
+
+function firstSixMovies(key) {
+  gallery.innerHTML = '';
+  const moviesFromLocalStorage = JSON.parse(localStorage.getItem(key));
+
+  if (moviesFromLocalStorage === null) {
+    gallery.classList.add('galleryEmptySpace');
+    titleNoMovie.classList.remove('display-none');
+    return;
+  } else {
+    gallery.classList.remove('galleryEmptySpace');
+    titleNoMovie.classList.add('display-none');
+  }
+  const updateInfoFroLocalStarage = sliceGanresDate(moviesFromLocalStorage);
+  const firstSix = updateInfoFroLocalStarage.slice(0, 6);
+  gallery.insertAdjacentHTML('beforeend', movieMarkup(firstSix));
+}
+function renewParam(num) {
+  visualNumberOfItems = num;
+  startIndex = num;
 }
